229260d615ab7219ec70b45911d0269921ad22b5
yjean-marie <yurijean-marie@live.com>
2020-01-11 23:25:16 -0400

    Minor Change

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {title: 'MedReg', message: 'Hello'});
+    res.render('index', {title: 'MedReg', message: 'Hello MedReg Team'});
 });
 
 module.exports = router;
